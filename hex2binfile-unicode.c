27d09dd004b912af745cad4f0f9b56fc16289042
ilya-re <rezenow75@gmail.com>
2025-05-23 03:46:12 +0300

    Upload the code

---
+++ hex2binfile-unicode.c
@@ -0,0 +1,53 @@
+#include <windows.h>
+#include <io.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+int wmain(int argc, wchar_t const *argv[]) {
+	// Parse command line arguments.
+	wchar_t const *InputFilename = NULL, *OutputFilename = NULL;
+	for (int i = 0; i < argc; i++) {
+		if (!wcscmp(argv[i], L"-i") && wcslen(argv[i + 1])) {InputFilename  = argv[i + 1];}
+		if (!wcscmp(argv[i], L"-o") && wcslen(argv[i + 1])) {OutputFilename = argv[i + 1];}
+	}
+	// If no filenames provided, print help and exit.
+	if (!(InputFilename && OutputFilename)) {
+		puts("Convert a text file containing hex data to a binary file\n"
+			 "The input file is case-insensitive, any non-hex characters are skipped\n"
+			 "-i <filename>  input file\n"
+			 "-o <filename> output file\n");
+		return 0;
+	}
+	// Open the input and output files.
+	errno_t ErrorCode;
+	FILE *InputFile = NULL, *OutputFile = NULL;
+	if (ErrorCode = _wfopen_s(&InputFile, InputFilename, L"rtS")) {
+		perror("Failed to open the input file");
+		return ErrorCode;
+	}
+	if (ErrorCode = _wfopen_s(&OutputFile, OutputFilename, L"wbS")) {
+		perror("Failed to create the output file");
+		return ErrorCode;
+	}
+	// Convert the text file to a binary one.
+	unsigned int BytesWritten = 0;
+	bool IsFirstDigit = true;
+	char ConvertBuf[3] = {0, 0, 0};
+	for (int InputChar = fgetc(InputFile); InputChar != EOF; InputChar = fgetc(InputFile)) {
+		if (!isxdigit(InputChar)) { continue; }				// Skip over non-hex characters.
+		if (IsFirstDigit) {ConvertBuf[0] = InputChar;}		// Put the first hex digit into the buffer.
+		else {
+			ConvertBuf[1] = InputChar;						// Put the second hex digit into the buffer.
+			uint8_t Byte = strtol(ConvertBuf, NULL, 16);	// Convert the hex string to a byte.
+			fwrite(&Byte, 1, 1, OutputFile);				// Write the byte into the output file.
+			BytesWritten++;
+		}
+		IsFirstDigit = !IsFirstDigit;
+	}
+	fclose(InputFile);
+	fclose(OutputFile);
+	printf("%u bytes written\n", BytesWritten);
+	return 0;
+}
---
+++ hex2binfile.c
@@ -0,0 +1,53 @@
+#include <windows.h>
+#include <io.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+	// Parse command line arguments.
+	char const *InputFilename = NULL, *OutputFilename = NULL;
+	for (int i = 0; i < argc; i++) {
+		if (!strcmp(argv[i], "-i") && strlen(argv[i + 1])) {InputFilename  = argv[i + 1];}
+		if (!strcmp(argv[i], "-o") && strlen(argv[i + 1])) {OutputFilename = argv[i + 1];}
+	}
+	// If no filenames provided, print help and exit.
+	if (!(InputFilename && OutputFilename)) {
+		puts("Convert a text file containing hex data to a binary file\n"
+			 "The input file is case-insensitive, any non-hex characters are skipped\n"
+			 "-i <filename>  input file\n"
+			 "-o <filename> output file\n");
+		return 0;
+	}
+	// Open the input and output files.
+	errno_t ErrorCode;
+	FILE *InputFile = NULL, *OutputFile = NULL;
+	if (ErrorCode = fopen_s(&InputFile, InputFilename, "rtS")) {
+		perror("Failed to open the input file");
+		return ErrorCode;
+	}
+	if (ErrorCode = fopen_s(&OutputFile, OutputFilename, "wbS")) {
+		perror("Failed to create the output file");
+		return ErrorCode;
+	}
+	// Convert the text file to a binary one.
+	unsigned int BytesWritten = 0;
+	bool IsFirstDigit = true;
+	char ConvertBuf[3] = {0, 0, 0};
+	for (int InputChar = fgetc(InputFile); InputChar != EOF; InputChar = fgetc(InputFile)) {
+		if (!isxdigit(InputChar)) { continue; }				// Skip over non-hex characters.
+		if (IsFirstDigit) {ConvertBuf[0] = InputChar;}		// Put the first hex digit into the buffer.
+		else {
+			ConvertBuf[1] = InputChar;						// Put the second hex digit into the buffer.
+			uint8_t Byte = strtol(ConvertBuf, NULL, 16);	// Convert the hex string to a byte.
+			fwrite(&Byte, 1, 1, OutputFile);				// Write the byte into the output file.
+			BytesWritten++;
+		}
+		IsFirstDigit = !IsFirstDigit;
+	}
+	fclose(InputFile);
+	fclose(OutputFile);
+	printf("%u bytes written\n", BytesWritten);
+	return 0;
+}
